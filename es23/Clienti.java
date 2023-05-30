5b6b15072e9b751c847c1a69f3f26bc0723a2813
baritonomattia <119524234+baritonomattia@users.noreply.github.com>
2023-05-30 10:45:16 +0000

    vfv

--- es23/Clienti.java
+++ es23/Clienti.java
@@ -1,5 +1,6 @@
 package es23;
 
+import java.util.ArrayList;
 import java.util.TreeMap;
 
 public class Clienti {
@@ -9,9 +10,24 @@ public class Clienti {
         cliente = new TreeMap<Integer, Cliente>(); 
     }
 
-    public void aggiungi(Cliente cliente) throws ClienteEsistente{
+    public void aggiungi(Cliente cliente) throws ClienteNonEsiste {
         if(!cliente.containsKey(cliente.getCodiceCliente())){
-            cliente.put
+            cliente.put(cliente.getCodiceCliente(), cliente.clone()); 
+        
+        }else{
+            throw new ClienteNonEsiste(); 
+        }
+    }
+
+    public void elimina(String codice) throws ClienteNonEsiste{
+        if(cliente.containsKey(codice)) return cliente.get(codice).clone(); 
+        else throw new ClienteNonEsiste(); 
+    }
+
+    public ArrayList<Cliente> trovaCliente(String numero){
+        ArrayList<Cliente> cliente_numero = new ArrayList<Cliente>(cliente.size()); 
+        for(Cliente cliente : cliente.values()){
+            if(cliente.get)
         }
     }
 }
